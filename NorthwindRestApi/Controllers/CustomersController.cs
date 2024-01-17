54c492216cc84629fa1fa7a30b5ae91c47092268
Artturi Arajarvi <Artturi.arajarvi@gmail.com>
2024-01-17 13:19:59 +0200

    Lisäystoiminto

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -14,8 +14,31 @@ namespace NorthwindRestApi.Controllers
         
         public ActionResult GetAllCustomers()
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää" + ex.InnerException);
+            }
+        }
+
+
+        [HttpGet("{id}")]
+
+        public ActionResult GetOneCustomersById(string id)
+        {
+            var asiakas = db.Customers.Find(id);
+            if(asiakas != null)
+            {
+                return Ok(asiakas);
+            }
+            else
+            {
+                return BadRequest($"Asiakasta id:llä {id} ei löydy.");
+            }
         }
     }
 }
